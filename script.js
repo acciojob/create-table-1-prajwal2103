a5791baaf417c189e4556b7ce56810a738538299
prajwal2103 <137304916+prajwal2103@users.noreply.github.com>
2024-01-06 19:19:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-let grandParent=document.getElementById("sampleTable");
+let grandParent=document.querySelector("tbody");
 function insert_Row() {
  let tr=document.createElement("tr");
  let td=document.createElement("td");
-	td.innerText="New Cell1"
-  tr.appendChild(td);
+  let td2=document.createElement("td");
+	td.innerText="New Cell1";
+	td2.innerText="New Cell2"
+    tr.append(td,td2);
 	grandParent.appendChild(tr)
 	let firstChild=document.querySlector("tr");
 	grandParent.insertBefore(tr,firstChild);
